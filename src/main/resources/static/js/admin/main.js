9e85c22d7c2d047b0e4cebebea2ef4c98e869a6a
303412768 <303412768@qq.com>
2018-09-21 18:27:20 +1000

    ui

--- src/main/resources/static/js/admin/main.js
+++ src/main/resources/static/js/admin/main.js
@@ -1,3 +1,10 @@
 loadPage = function (url) {
     $("#main_content").load(url)
 };
+
+loadPageByModal = function (url) {
+    $.get(url, function (result) {
+        $('#your-modal').html(result);
+        $('#your-modal').modal('open');
+    });
+};
